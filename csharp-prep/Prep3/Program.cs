adcbb1e623ebfd4fd7652d951fd4475f2a0bdf46
Orange4me <brandonalder@byui.edu>
2023-01-11 12:06:34 -0700

    yo

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 21);
+        int Guess = 21;
+        string guess = "21";
+
+        while (Guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            Guess = Int16.Parse(guess);
+
+
+            
+            if (Guess > number)
+            {
+            Console.WriteLine("Lower");
+            }
+            else if (Guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
